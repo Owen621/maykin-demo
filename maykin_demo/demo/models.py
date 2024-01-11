6896d0845283943d596926d210e0e3f910c471e5
Owen <freemanowen77@gmail.com>
2024-01-11 15:16:09 +0000

    complete

--- maykin_demo/demo/models.py
+++ maykin_demo/demo/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
-import uuid
 # Create your models here.
 
 class City (models.Model):
